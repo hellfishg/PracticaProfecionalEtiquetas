c7e50973ae38fbb2525485bac4862dd38f5d8f85
Francis <francis._-100@hotmail.com>
2017-10-29 19:06:07 -0300

    Agregado de codigo para AgregarPersona.aspx.cs
    Se añadio la funcion de poder agregar datos a la tabla de Personas

--- Personas/AgregarPersona.aspx.cs
+++ Personas/AgregarPersona.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,13 @@ namespace Sistema_de_etiquetas.Personas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void AgregarPRSNA_Click(object sender, EventArgs e)
+        {
+            Conexion cn = new Conexion();
+            cn.AgregarPersona(Convert.ToInt32(NroDoc.Text.ToString()), TipoDoc.Text, Apellido.Text, Nombre.Text, Provincia.Text, Localidad.Text, Direccion.Text, Convert.ToInt32(Telefono.Text.ToString()), Convert.ToInt32(Celular.Text.ToString()), Email.Text, Sexo.Text, EstadoCivil.Text, Nacionalidad.Text);
         }
     }
 }
