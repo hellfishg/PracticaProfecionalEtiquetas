87bca6b02a2de07848145993277bae745ab41464
Francis <francis._-100@hotmail.com>
2017-10-29 18:51:33 -0300

    Agregado de codigo para VerDatosCursos.aspx.cs
    Se añadio la funcion de mostrar la tabla de cursos

--- Cursos/VerDatosCurso.aspx.cs
+++ Cursos/VerDatosCurso.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,11 @@ namespace Sistema_de_etiquetas.Cursos
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                Conexion cn = new Conexion();
+                cn.MostrarDatosCursos(grdVerCursos);
+            }
         }
     }
 }
