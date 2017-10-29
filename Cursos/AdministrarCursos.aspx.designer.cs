a4966f0f76faee41b0a10cfeae32e91ec6ac9c25
Francis <francis._-100@hotmail.com>
2017-10-29 18:42:59 -0300

    Actualizar AdministrarCursos.aspx.designer.cs
    Debido al cambio de ID en gridview se actualiza la ID en AdministrarCursos.aspx.designer.cs

--- Cursos/AdministrarCursos.aspx.designer.cs
+++ Cursos/AdministrarCursos.aspx.designer.cs
@@ -1,5 +1,7 @@
 ﻿//------------------------------------------------------------------------------
 // <generado automáticamente>
+//------------------------------------------------------------------------------
+// <generado automáticamente>
 //     Este código fue generado por una herramienta.
 //
 //     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
@@ -13,12 +15,12 @@ namespace Sistema_de_etiquetas.Cursos {
     public partial class AdministrarCursos {
         
         /// <summary>
-        /// Control GridView1.
+        /// Control grdAdministrarCursos.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView grdAdministrarCursos;
     }
 }
