37dbc5f21e8cd9901ad5aacecccd0c50eb2cca2a
emanuelbatalla <emanuel_batalla@hotmail.com>
2017-11-21 19:00:56 -0300

    Arreglo de bugs "agregar materia"

--- Materias/AgregarMateria.aspx.cs
+++ Materias/AgregarMateria.aspx.cs
@@ -24,24 +24,27 @@ namespace Sistema_de_etiquetas.Materias
             bool aux=false;
             Conexion cn = new Conexion();
 
-            if (TextBox2.Text.ToString().Trim().Length > 3 || TextBox2.Text.ToString().Trim().Length < 1)
+            if (txtCodigo.Text.ToString().Trim().Length > 3 || txtCodigo.Text.ToString().Trim().Length < 1)
             {
                 Response.Write("<script>window.alert('El codigo debe ser de al menos 1 caracter y menos de 4');</script>");
                 aux = true;}
 
-            if (TextBox4.Text.ToString().Trim() == "") { Response.Write("<script>window.alert('Ingrese tipo de plan');</script>");
+            if (txtTipoPlan.Text.ToString().Trim() == "")
+            {
+                Response.Write("<script>window.alert('Ingrese tipo de plan');</script>");
             aux = true;}
 
-            if (TextBox3.Text.ToString().Trim() == "")
+            if (txtNombre.Text.ToString().Trim() == "")
             {   Response.Write("<script>window.alert('Ingrese Nombre de materia');</script>");
                 aux = true;}
 
-            if (cn.ValidarClaveMateria(TextBox2.Text.ToString().Trim()) == true){
+            if (cn.ValidarClaveMateria(txtCodigo.Text.ToString().Trim()) == true)
+            {
                 Response.Write("<script>window.alert('La clave está repetida');</script>");
                 aux = true;}
 
-            if(aux == false) cn.AgregarMateria(ddlcarreras.SelectedItem.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, ddlCursadas.SelectedItem.Text);
-            cn.AgregarMateria(ddlcarreras.SelectedItem.Text, txtCodigo.Text, txtNombre.Text, txtTipoPlan.Text, ddlCursadas.SelectedItem.Text);
+            if (aux == false) cn.AgregarMateria(ddlcarreras.SelectedItem.Text, txtCodigo.Text, txtNombre.Text, txtTipoPlan.Text, ddlCursadas.SelectedItem.Text);
+            Response.Write("<script>window.alert('Materia agregada con exito');</script>");
             
         }
     }
